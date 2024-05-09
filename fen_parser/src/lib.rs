85fc285e1a3927b8c06cca1ac48215f1edc6e4dd
Kyle Krowpman <kyle.krowpman@gmail.com>
2024-05-08 17:08:21 -0700

    remove dependencies

--- fen_parser/src/lib.rs
+++ fen_parser/src/lib.rs
@@ -1,35 +1,74 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-use chess::{Board, Piece, Color};
-use std::str::FromStr;
+
+const PAWN: u8 = 0;
+const KNIGHT: u8 = 1;
+const BISHOP: u8 = 2;
+const ROOK: u8 = 3;
+const QUEEN: u8 = 4;
+const KING: u8 = 5;
+
+const WHITE: u8 = 0;
+const BLACK: u8 = 6;
+
+fn index(pc_char: u8, rank: u8, file: u8) -> usize {
+    let (pt, color) = match pc_char {
+        b'K' => (KING, WHITE),
+        b'k' => (KING, BLACK),
+
+        b'Q' => (QUEEN, WHITE),
+        b'q' => (QUEEN, BLACK),
+
+        b'R' => (ROOK, WHITE),
+        b'r' => (ROOK, BLACK),
+
+        b'B' => (BISHOP, WHITE),
+        b'b' => (BISHOP, BLACK),
+
+        b'N' => (KNIGHT, WHITE),
+        b'n' => (KNIGHT, BLACK),
+
+        b'P' => (PAWN, WHITE),
+        b'p' => (PAWN, BLACK),
+        _ => {
+            panic!("Unrecognized piece type: {}.", pc_char);
+        }
+    };
+    let pc = (color + pt) as usize;
+    let sq = ((rank << 3) ^ file) as usize;
+    64 * pc + sq
+}
 
 #[pyfunction]
 fn fen_to_vec(fen: &str) -> PyResult<Vec<u8>> {
-    let mut vector = vec![0u8; 768];
-    let board = Board::from_str(fen).expect("Invalid fen.");
-    let white = board.color_combined(Color::White);
-    let black = board.color_combined(Color::Black);
-    let bitboards = [
-        board.pieces(Piece::Pawn) & white,
-        board.pieces(Piece::Knight) & white,
-        board.pieces(Piece::Bishop) & white,
-        board.pieces(Piece::Rook) & white,
-        board.pieces(Piece::Queen) & white,
-        board.pieces(Piece::King) & white,
-        board.pieces(Piece::Pawn) & black,
-        board.pieces(Piece::Knight) & black,
-        board.pieces(Piece::Bishop) & black,
-        board.pieces(Piece::Rook) & black,
-        board.pieces(Piece::Queen) & black,
-        board.pieces(Piece::King) & black,
-    ];
+    let mut inputs: Vec<u8> = vec![0u8; 768];
+    let mut rank = 7;
+    let mut file = 0;
 
-    for (index, piece_bb) in bitboards.iter().enumerate() {
-        for sq in *piece_bb {
-            vector[index*64 + sq.to_int() as usize] = 1;
+    let position = match fen.split(' ').next() {
+        Some(s) => String::from(s),
+        None => panic!("Malformed FEN."),
+    };
+    for char in position.as_bytes().iter() {
+        let char = *char;
+        match char {
+            b'A'..=b'z' => {
+                inputs[index(char, rank, file)] = 1;
+                file += 1;
+            }
+            b'/' => {
+                rank -= 1;
+                file = 0;
+            }
+            b'1'..=b'8' => {
+                file += char - b'0';
+            }
+            _ => {
+                panic!("Unrecognized character.");
+            }
         }
     }
-    Ok(vector)
+    Ok(inputs)
 }
 
 #[pymodule]
